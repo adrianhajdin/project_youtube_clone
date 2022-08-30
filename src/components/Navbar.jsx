6d2e7dd757c063fa007a2d1c481a92cc8ff04bf7
Eric Bourcier-Walsh <fatkungfu@gmail.com>
2022-08-30 14:14:07 -0400

    fix logo link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { SearchBar } from '.';
 
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}>
-    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+    <Link to="/" reloadDocument style={{ display: 'flex', alignItems: 'center' }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
     <SearchBar />
