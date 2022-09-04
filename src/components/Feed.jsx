e1b541fe7cc5ffd6f2f0d188a1941c622a551a6c
Sumon Hossain <sumonhosainrakib@hotmail.com>
2022-09-04 15:44:39 +0600

    Update Feed.jsx
    Hi... I just fixed some minor bugs

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -16,8 +16,8 @@ const Feed = () => {
     }, [selectedCategory]);
 
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+    <Stack sx={{ flexDirection: { xs: "column", md: "row" } }}>
+      <Box sx={{ height: { xs: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { xs: 0, md: 2 } }}>
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
