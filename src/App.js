f0ca35ae0ee85ea21a5f058620d3370972bb90b0
anuj <bhoranuj6@gmail.com>
2022-09-28 19:09:32 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
 
-import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+import {
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
+  Navbar,
+  Feed,
+} from "./components";
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{ backgroundColor: '#000' }}>
+    <Box sx={{ backgroundColor: "#111" }}>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Feed />} />
-        <Route path='/video/:id' element={<VideoDetail />} />
-        <Route path='/channel/:id' element={<ChannelDetail />} />
-        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
-import { logo } from "../utils/constants";
+import logo from "../assets/logo.png";
 import { SearchBar } from "./";
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#111",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
+      <img src={logo} alt="logo" height={45} width={70} />
     </Link>
     <SearchBar />
   </Stack>
