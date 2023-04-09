1684a5043f7d043cdca79b518c6ccf8ea80c98ce
Khateeb Anwer <khateeb_anwer123@hotmail.com>
2023-04-09 23:46:37 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,6 +4,7 @@ export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 let sorting = ["date","relevance","rating","title","viewCount"];
 let sortingIndex = Math.floor(Math.random() * sorting.length);
+
 let countryCodes = ["AF", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS",
                    "BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN",
                    "BG", "BF", "BI", "CV", "KH", "CM", "CA", "KY", "CF", "TD", "CL", "CN", "CX", "CC", "CO", "KM", "CD",
@@ -18,9 +19,9 @@ let countryCodes = ["AF", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", 
                    "KN", "LC", "MF", "PM", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", 
                    "SB", "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH", 
                    "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "UM", "US", "UY", 
-                   "UZ", "VU", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW", "AX"]
-let countryIndex = Math.floor(Math.random()*countryCodes.length);
+                   "UZ", "VU", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW", "AX"];
 
+let countryIndex = Math.floor(Math.random()*countryCodes.length);
 
 const options = {
   params: {
