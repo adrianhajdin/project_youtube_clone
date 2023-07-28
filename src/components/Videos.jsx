f661a1eaab423303ed641cc014dfdc235bd9ba02
jiexu <linj121@mcmaster.ca>
2023-07-27 22:36:46 -0400

    fix: remove invalid data to avoid rendering empty html nodes

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -6,9 +6,11 @@ import { ChannelCard, Loader, VideoCard } from "./";
 const Videos = ({ videos, direction }) => {
   if(!videos?.length) return <Loader />;
   
+  const validVideos = videos.filter(item => item.id.videoId || item.id.channelId);
+
   return (
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
-      {videos.map((item, idx) => (
+      {validVideos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} /> }
           {item.id.channelId && <ChannelCard channelDetail={item} />}
